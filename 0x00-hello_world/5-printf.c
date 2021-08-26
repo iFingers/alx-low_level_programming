5f259233fe60fa3304a231eba9a6c428e8a07e17
Abu Jehoiada <rodney20102@gmail.com>
2021-08-26 11:53:39 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Beginning of code
+ * Description:Begining of code
+ * Return: void
+ */
+int main(void){
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
