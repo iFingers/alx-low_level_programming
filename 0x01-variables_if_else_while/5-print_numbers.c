5bac91346e9a4ad1af655e0afa6b042549f8f3b0
Abu Jehoiada <rodney20102@gmail.com>
2021-08-30 11:21:56 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,13 +7,11 @@
 int main(void)
 {
 int n;
-  /*  For loop
-  (initialize; condition ; increment) */
-for(n = 0; n < 10; n++)
+for (n = 0; n < 10; n++)
 {
-printf("%d",n);
+printf("%d", n);
 }
 printf("\n");
 
- return (0);
+return (0);
 }
