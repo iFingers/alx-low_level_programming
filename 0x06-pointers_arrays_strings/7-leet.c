090948b7d0140f2c9f474eb8608c5402ed3af6b0
Abu Jehoiada <rodney20102@gmail.com>
2021-09-09 12:07:30 -0700

    Leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,10 +3,27 @@
  * leet - For printing letters into digits
  *@s: Parameter used
  *
- *no return since its void
+ *Return: Value of s
  */
 
-char *leet(char *)
+char *leet(char *s)
 {
+	int i;
+	char a[5] = {'A', 'E', 'O', 'T', 'L'};
+	char b[5] = {'4', '3', '0', '7', '1'};
+	int c;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (c = 0; c < 5; c++)
+		{
+			if (s[i] == a[c] || s[i] - 32 == a[c])
+				{
+					s[i] = b[c];
+				}
+		}
+	}
+
+
+return (s);
 }
