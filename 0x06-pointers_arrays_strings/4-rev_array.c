bc6c69476721d105ee550256c66ab5e836e69c8b
Abu Jehoiada <rodney20102@gmail.com>
2021-09-09 11:15:33 -0700

    Second commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,11 +11,13 @@
 void reverse_array(int *a, int n)
 {
 	int i;
-	for (i = 0; i < (n / 2); i++)
-	{
-	    int swap = a[n - 1 - i];
-	    a[n - 1 - i] = a[i];
-	    a[i] = swap;
-	}
+
+for (i = 0; i < (n / 2); i++)
+{
+	int swap = a[n - 1 - i];
+
+	a[n - 1 - i] = a[i];
+	a[i] = swap;
+}
 
 }
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,6 +3,7 @@
 #include <string.h>
 /**
  *string_toupper - For changing strings to upper case
+ *@str: pointer of string parameter to be changed
  *
  *Return: string in upper case
  */
@@ -11,14 +12,14 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 
-       	while(*(str + i)!= '\0')
+	while (*(str + i) != '\0')
+{
+	if (*(str + i) >= 97 && *(str + i) <= 122)
 	{
-		if(*(str+i) >= 97 && *(str + i) <= 122)
-		{
-			*(str + i) = *(str + i) - 32;
-		}
-		i++;
+		*(str + i) = *(str + i) - 32;
 	}
+	i++;
+}
 
-	return (str);
+return (str);
 }
